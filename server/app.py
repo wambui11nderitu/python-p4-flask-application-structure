a2be5728e053793badc8b55badc15acc2b0be35e
wambui11nderitu <wambuinderitu93@gmail.com>
2024-07-19 15:54:20 +0300

    updating labs

--- server/app.py
+++ server/app.py
@@ -3,3 +3,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome , to my page!</h1>'
+@app.route('/string:username>')
+def hello_user(username):
+    return f'<h1>Welcome, {username}!</h1>'
